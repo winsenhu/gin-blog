2e1374f16fc866fdb3442ac19b09478601917048
huhs <149571639@qq.com>
2021-01-11 19:51:59 +0800

    add tag update delete

--- models/tag.go
+++ models/tag.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+    "time"
+
+    "github.com/jinzhu/gorm"
+)
+
 type Tag struct {
     Model
 
@@ -39,4 +45,38 @@ func AddTag(name string, state int, createdBy string) bool{
     })
 
     return true
+}
+
+func ExistTagByID(id int) bool {
+    var tag Tag
+    db.Select("id").Where("id = ?", id).First(&tag)
+    if tag.ID > 0 {
+        return true
+    }
+
+    return false
+}
+
+func DeleteTag(id int) bool {
+    db.Where("id = ?", id).Delete(&Tag{})
+
+    return true
+}
+
+func EditTag(id int, data interface {}) bool {
+    db.Model(&Tag{}).Where("id = ?", id).Updates(data)
+
+    return true
+}
+
+func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
+    scope.SetColumn("CreatedOn", time.Now().Unix())
+
+    return nil
+}
+
+func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
+    scope.SetColumn("ModifiedOn", time.Now().Unix())
+
+    return nil
 }
--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -74,8 +74,69 @@ func AddTag(c *gin.Context) {
 
 //修改文章标签
 func EditTag(c *gin.Context) {
-}
+    id := com.StrTo(c.Param("id")).MustInt()
+    name := c.Query("name")
+    modifiedBy := c.Query("modified_by")
+
+    valid := validation.Validation{}
+
+    var state int = -1
+    if arg := c.Query("state"); arg != "" {
+        state = com.StrTo(arg).MustInt()
+        valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
+    }
+
+    valid.Required(id, "id").Message("ID不能为空")
+    valid.Required(modifiedBy, "modified_by").Message("修改人不能为空")
+    valid.MaxSize(modifiedBy, 100, "modified_by").Message("修改人最长为100字符")
+    valid.MaxSize(name, 100, "name").Message("名称最长为100字符")
+
+    code := e.INVALID_PARAMS
+    if ! valid.HasErrors() {
+        code = e.SUCCESS
+        if models.ExistTagByID(id) {
+            data := make(map[string]interface{})
+            data["modified_by"] = modifiedBy
+            if name != "" {
+                data["name"] = name
+            }
+            if state != -1 {
+                data["state"] = state
+            }
+
+            models.EditTag(id, data)
+        } else {
+            code = e.ERROR_NOT_EXIST_TAG
+        }
+    }
+
+    c.JSON(http.StatusOK, gin.H{
+        "code" : code,
+        "msg" : e.GetMsg(code),
+        "data" : make(map[string]string),
+    })
+}    
 
 //删除文章标签
 func DeleteTag(c *gin.Context) {
+    id := com.StrTo(c.Param("id")).MustInt()
+
+    valid := validation.Validation{}
+    valid.Min(id, 1, "id").Message("ID必须大于0")
+
+    code := e.INVALID_PARAMS
+    if ! valid.HasErrors() {
+        code = e.SUCCESS
+        if models.ExistTagByID(id) {
+            models.DeleteTag(id)
+        } else {
+            code = e.ERROR_NOT_EXIST_TAG
+        }
+    }
+
+    c.JSON(http.StatusOK, gin.H{
+        "code" : code,
+        "msg" : e.GetMsg(code),
+        "data" : make(map[string]string),
+    })
 }
